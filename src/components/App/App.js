9e6c00675c90b85f6d54a891090c4170fd07ab8c
Steven Lawson <unluckygamblermusic@gmail.com>
2018-03-20 17:45:05 -0600

    adding names, need to fetch again for species

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import OverViewCrawl from '../OverViewCrawl/OverViewCrawl';
 import './App.css';
-import ButtonContainer from '../ButtonContainer/ButtonContainer'
+import ButtonContainer from '../ButtonContainer/ButtonContainer';
+import CardContainer from '../CardContainer/CardContainer';
+// import dataCleaner from '../helper';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starWars: []
+      starWars: null
 
     }
+  }
+  fetchSpecies = (starWars) => {
+    console.log(starWars.results)
+    const promises = starWars.results.map(person => {
+      console.log('perosn',person.species)
+      return fetch(person.species)
+      .then(response => response.json())
+      .then(data => ({name: person.species}))
+    })
+    return Promise.all(promises)
+  }
+
+  componentDidMount() {
+    // .then(data => this.fetchBios(data.bio))
+
+    const url = 'https://swapi.co/api/people/'
+    fetch(url)
+    .then(response => response.json())
+    // .then( starWars => this.fetchSpecies(starWars))
+    .then(starWars => this.setState({ starWars }))
+    // .then(staff => this.setState({ staff }))
   }
   render() {
     return (
@@ -19,6 +42,10 @@ class App extends Component {
         </header>
 
         <OverViewCrawl />
+        {
+          this.state.starWars &&
+          <CardContainer data={this.state.starWars} />
+        }
         <ButtonContainer />
       </div>
     );
